37855ab84663472aa0ef83fb1e46ead3ce3ea1ed
Yegor Belov <georgy.belov@revolut.com>
2020-08-01 16:11:04 +0300

    setup react/testing-library

---
+++ src/test-utils.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "@chakra-ui/core";
+
+const customRender = (ui, { route = "/", store, ...options } = {}) => {
+  const AppProviders: React.FunctionComponent = ({ children }) => {
+    return (
+      <ThemeProvider>
+        <Router initialEntries={[route]}>{children}</Router>
+      </ThemeProvider>
+    );
+  };
+
+  return render(ui, {
+    wrapper: AppProviders,
+    ...options,
+  });
+};
+
+// re-export everything
+export * from "@testing-library/react";
+
+// override render method
+export { customRender as render };
